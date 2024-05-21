68c0997e8554fac01ac7c55f3dbdb535bf0ab9b2
Md.Samsul Arefin <samsularefin532@gmail.com>
2024-05-21 17:10:10 +0600

    product get by single it fethced properly

--- src/app/modules/product/productController.ts
+++ src/app/modules/product/productController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import productService from './productService';
 import productValidation from './productValidation';
+import productModel from './productModel';
 
 const createdProduct = async (req: Request, res: Response) => {
   const data = req.body;
@@ -21,7 +22,22 @@ const createdProduct = async (req: Request, res: Response) => {
     res.status(500).json({ message: err.message });
   }
 };
-const getProduct = (res: Response, req: Request) => {};
+const getProduct = async (req: Request, res: Response) => {
+  const { productId } = req.params;
+  let filter = {};
+  if (productId) {
+    try {
+      const result = await productModel.find({ _id: productId });
+      res.status(200).json({
+        success: true,
+        message: 'Product fetched successfully!',
+        data: result,
+      });
+    } catch (error) {
+      res.status(500).json({ success: false, message: error.message });
+    }
+  }
+};
 const updateProduct = (res: Response, req: Request) => {};
 const deleteProduct = (res: Response, req: Request) => {};
 
--- src/app/modules/product/productRoute.ts
+++ src/app/modules/product/productRoute.ts
@@ -2,8 +2,8 @@ import express from 'express';
 import productController from './productController';
 const productRoute = express.Router();
 
-productRoute.get('', productController.getProduct);
-productRoute.post('/', productController.createdProduct);
+productRoute.get('/:productId', productController.getProduct);
+productRoute.post('', productController.createdProduct);
 productRoute.put('', productController.updateProduct);
 productRoute.delete('', productController.deleteProduct);
 
